7a636441a75524c04efe882edb460a076db8754c
Anwar <anwar@aknpacitan.ac.id>
2021-09-12 19:54:22 +0700

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Booking;
+use App\Models\Customer;
+use App\Models\Room;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        return view('home');
+    public function index(){
+        $totalBooking = Booking::count();
+        $totalRoom = Room::count();
+        $totalCustomer = Customer::count();
+        $bookings = Booking::limit(5)->latest()->get();
+
+        $monthly = DB::select('SELECT * FROM booking_per_bulan');
+
+        return view('home', compact('totalBooking', 'totalRoom', 'bookings', 'totalCustomer', 'monthly'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,96 @@
 @section('title', 'Home')
 
 @section('breadcrumb')
-<li class="breadcrumb-item active"><a href="#">Home</a>
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active">Dashboard</li>
 @endsection
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <h3>Welcome</h3>
+    <div class="row">
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $totalBooking }}</h3>
+                    <p>Pemesanan</p>
+                </div>
+                <div class="icon"><i class="fas fa-shopping-cart"></i></div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $totalRoom }}</h3>
+                    <p>Produk</p>
+                </div>
+                <div class="icon"><i class="fas fa-th"></i></div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $totalCustomer }}</h3>
+                    <p>Customer</p>
+                </div>
+                <div class="icon"><i class="fas fa-users"></i></div>
+            </div>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Pemesanan Terakhir</div>
+                <div class="card-body">
+                    <table class="table table-stripped">
+                        <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nomer Pemesanan</th>
+                            <th>Pelanggan</th>
+                            <th>Pengguna</th>
+                            <th>Tanggal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($bookings as $booking)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $booking->booking_no }}</td>
+                                <td>{{ $booking->customer->customer_name }}</td>
+                                <td>{{ $booking->user->name }}</td>
+                                <td>{{ $booking->booking_date }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Pemesanan Per Bulan</div>
+                <div class="card-body">
+                    <table class="table table-stripped">
+                        <thead>
+                        <tr>
+                            <th>Bulan</th>
+                            <th>Tahun</th>
+                            <th>Jumlah Pesanan</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($monthly as $m)
+                            <tr>
+                                <td>{{ $m->bulan }}</td>
+                                <td>{{ $m->tahun }}</td>
+                                <td>{{ $m->jml_booking }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
